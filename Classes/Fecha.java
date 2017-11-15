39053941639542ce19454671c1fd5520abbd031e
unknown <serhuegi@gmail.com>
2017-11-16 00:59:35 +0100

    Interface changed, a simple login is made, readme.md in english and spanish

--- Classes/Fecha.java
+++ Classes/Fecha.java
@@ -4,8 +4,6 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
-import Framework.Utils.F;
-
 public class Fecha {
 	private String fecha;
 
--- Main/MainExe.java
+++ Main/MainExe.java
@@ -1,19 +1,32 @@
 package Framework.Main;
 
+import javax.swing.UIManager;
+
 import Framework.Modules.Utils.MakeDummies;
 import Framework.Utils.F;
 
 public class MainExe {
-	public static void exe(){
-		String[] options = {"Dummies", "No dummies"};
+	public static void exe() {
+		String[] options = { "Dummies", "No dummies" };
 		F.iniSingleton();
+		String usu = "";
+		String pass = "";
+		try {
+			UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+		} catch (Exception e) {
+
+		}
+		do {
+			usu = F.askString("Introduce your username");
+			pass = F.askString("Introduce your password");
+		} while (!usu.equals("yomogan") && !pass.equals("yomogan"));
 		int dummies = F.menubuttons(options, "Do you want to use dummies?", "Main");
-		if(dummies == 0){
+		if (dummies == 0) {
 			MakeDummies.Dummies();
 			Main.exe();
-		}else if(dummies == 1){
+		} else if (dummies == 1) {
 			Main.exe();
-		}else
+		} else
 			System.exit(0);
 	}
 }
